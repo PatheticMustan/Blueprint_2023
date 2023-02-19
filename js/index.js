11813b655f7f22b15379ac7b4402badafd224e8f
hyousfi2006 <hyousfi2006@gmail.com>
2023-02-19 13:16:38 -0600

    mvllrkfefv

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // code to make textarea auto-resize
-const tx = document.getElementsByTagName("textarea");
+const tx = document.getElementsByTagName("textarea")[0];
 for (let i = 0; i < tx.length; i++) {
   tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
   tx[i].addEventListener("input", OnInput, false);
@@ -8,4 +8,8 @@ for (let i = 0; i < tx.length; i++) {
 function OnInput() {
   this.style.height = 0;
   this.style.height = (this.scrollHeight) + "px";
+}
+
+function submit() {
+  tx.readOnly=true
 }
