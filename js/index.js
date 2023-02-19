f11221694a1df5caf4de1adbcec96cc9bfda2e14
Mustan Pathetic <patheticmustan@gmail.com>
2023-02-19 18:31:33 +0000

    fix readonly after submit

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 document.onload = () => {
-  // code to make textarea auto-resize
-  const rantbox = document.getElementById("rantbox");
-  for (let i=0; i<rantbox.length; i++) {
+    // code to make textarea auto-resize
+    const rantbox = document.getElementById("rantbox");
     rantbox[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
-    rantbox[i].addEventListener("input", OnInput, false);
-  }
+    rantbox[i].addEventListener("input", onInput, false);
 
-  function OnInput() {
+    
+}
+
+function onInput() {
     this.style.height = 0;
     this.style.height = (this.scrollHeight) + "px";
-  }
-  function submit() {
-    tx.readOnly=true
-  }
+}
+
+function submit() {
+    rantbox.readOnly = true;
 }
