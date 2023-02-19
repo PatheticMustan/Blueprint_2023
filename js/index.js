17d288380db4062c246f46da3ba31703f391e21b
Mustan Pathetic <patheticmustan@gmail.com>
2023-02-19 16:15:06 +0000

    meta tags, stop textarea drag

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// code to make textarea auto-resize
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = 0;
+  this.style.height = (this.scrollHeight) + "px";
+}
