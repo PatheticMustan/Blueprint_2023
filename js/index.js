c57a1ddcb4d1eb3ab828774d7743dfdc9a95bb2a
Mustan Pathetic <patheticmustan@gmail.com>
2023-02-19 21:32:06 +0000

    checkpoint

--- js/index.js
+++ js/index.js
@@ -1,28 +1,25 @@
-document.onload = () => {
-    // code to make textarea auto-resize
-    const rantbox = document.getElementById("rantbox");
-    rantbox.setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
-    rantbox.addEventListener("input", onInput, false);
-
-    
-}
+// code to make textarea auto-resize
+const rantbox = document.getElementById("rantbox");
+rantbox.setAttribute("style", "height:" + (rantbox.scrollHeight) + "px;overflow-y:hidden;");
+rantbox.addEventListener("input", onInput, false);
 
 function onInput() {
-    alert(1);
     this.style.height = 0;
-    this.style.height = (this.scrollHeight) + "px";
+    this.style.height = Math.min((this.scrollHeight) + "px";
 }
 
 function submit() {
     rantbox.readOnly = true;
-    const response=document.getElementById("response");
-    let advice="";
+    const response = document.getElementById("response");
+    let advice = "";
+    
     Object.keys(keywords).map(word => {
         if (rantbox.value.indexOf(word) !== -1) {
             advice = advice + keywords[word] + "\n"
         }
-    })
-    response.innerHTML="Rock: "+advice;
+    });
+    
+    response.innerHTML = "Rock: " + (advice || "[...]");
 }
 
 let keywords = {
@@ -38,7 +35,19 @@ let keywords = {
     "chaotic":"Modern day life can be extremely stressful and chaotic, but its important to keep striving towards your goal and believing in yourself!",
     "chill":"Although it is good to take rest days, make sure to come back and focus on whatever goals you have planned!",
     "clingy":"Make sure to assert your boundaries and preferences in a relationship.",
-  
-  
-  
+    "jealous":"Focus on yourself. Theres no point in dwelling over things other people have.",
+    "difficult":"Its good to take a break, but make sure you know when to continue on! Forward soldier, march on! Remember, you can do this!",
+    "struggling":"Keep working hard! I know you can do it! Just remember to take a break once in a while.",
+    "hate":"Its okay not to like everything all the time. Sometimes you just have to be okay, with having your own opinions.",
+    "horrible":"Its okay to have a bad day. You do not have to be happy 24/7. Keep in mind that you are human!",
+    "give_up":"Have a little more confidence! Keeping a positive mindset will make things easier for you to do. Dont give up so soon! Keep trying.",
+    "depressed":"Life gets tough, but that okay. Keep working through it! You've",
+    "busy":"Be sure that you have time for yourself! Try and keep yourself organized so that things dont get too much for you.",
+    "tired":"I know things can be exhausting, be sure to take breaks and refresh your head.",
+    "exhausted":"I see that you're tired. Make sure to get some rest and continue on once you are in the right mental space.",
+    "":"",
+    "":"",
+    "":"",
+    "":"",
+    "":"",
   }
